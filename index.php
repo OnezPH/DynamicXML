81af5c308efecb7ebd6f95f8156493c2b9a1e8ec
OnezPH <109589426+OnezPH@users.noreply.github.com>
2022-10-14 16:24:13 +0800

    dynamicXML

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 $xml=simplexml_load_file("dynamicXML.xml") or die("Error: Cannot create object");
-echo $xml->link->company . "<br>";
+echo $xml->link[0]->company . "<br>";
+echo $xml->link[0]->product . "<br>";
+echo $xml->link[0]->prices . "<br>";
+echo $xml->link[0]->image . "<br>";
+echo $xml->link[0]->url;
 ?>
