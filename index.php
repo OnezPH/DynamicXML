86eeb6471eff42fb47f069f113c9bf6337ba0a1e
OnezPH <109589426+OnezPH@users.noreply.github.com>
2022-10-14 16:29:35 +0800

    dynamicXML

--- index.php
+++ index.php
@@ -4,5 +4,17 @@ echo $xml->link[0]->company . "<br>";
 echo $xml->link[0]->product . "<br>";
 echo $xml->link[0]->prices . "<br>";
 echo $xml->link[0]->image . "<br>";
-echo $xml->link[0]->url;
+echo $xml->link[0]->url . "<br>" . "<br>";
+
+echo $xml->link[1]->company . "<br>";
+echo $xml->link[1]->product . "<br>";
+echo $xml->link[1]->prices . "<br>";
+echo $xml->link[1]->image . "<br>";
+echo $xml->link[1]->url . "<br>" . "<br>";
+
+echo $xml->link[2]->company . "<br>";
+echo $xml->link[2]->product . "<br>";
+echo $xml->link[2]->prices . "<br>";
+echo $xml->link[2]->image . "<br>";
+echo $xml->link[2]->url . "<br>" . "<br>";
 ?>
